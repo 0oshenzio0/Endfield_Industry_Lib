b3f025daebbc2f7a8594c053ad8812d3ac1da1eb
0oshenzio0 <2942682815@qq.com>
2026-07-20 17:17:54 +0800

    构建星系 构建了塔罗斯和塔卫二，塔卫二地形尚未规定，塔罗斯目前绘制了绕日轨道

--- src/endfieldindustrylib/Lib.java
+++ src/endfieldindustrylib/Lib.java
@@ -2,6 +2,7 @@ package endfieldindustrylib;
 
 import endfieldindustrylib.EFcontents.EFblocks;
 import endfieldindustrylib.EFcontents.EFitems;
+import endfieldindustrylib.EFcontents.EFplanets;
 
 public class Lib extends mindustry.mod.Mod {
 
@@ -9,6 +10,8 @@ public class Lib extends mindustry.mod.Mod {
     public void loadContent() {
         // item
         EFitems.load();
+
+        EFplanets.loadContents();
 
         // block
         EFblocks.load();
